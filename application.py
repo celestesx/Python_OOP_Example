c44e4a0dce42e84d946e9d17c4156323d04a32db
Liane Wong <89564832+celestesx@users.noreply.github.com>
2023-01-25 18:27:50 +0800

    Create application.py

---
+++ application.py
@@ -0,0 +1,9 @@
+import backend
+import frontend
+
+class Application:
+    def __init__(self):
+        self.__backend = backend.BackEnd("data.csv")
+        self.__frontend = frontend.FrontEnd(self.__backend)
+
+app = Application()
